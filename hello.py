af02eaf63458d91c5af0faf6f77751893e09421a
PabloMunizMontoya <119509495+PabloMunizMontoya@users.noreply.github.com>
2023-06-30 02:23:12 -0500

    variables

---
+++ hello.py
@@ -0,0 +1,9 @@
+# hola mundo
+print("Hello Python")
+
+# consultar tipo de datos 
+print(type('Hola Python que dato eres')) # str es string
+print(type(5)) # int es un numero entero
+print(type(5.1)) # float
+print(type(True)) # bool
+
---
+++ variables.py
@@ -0,0 +1,35 @@
+# variables
+
+my_variable = 'variable 1' # se declaran asi snake_case
+print(my_variable)
+
+my_string_variable = 'str'
+
+my_int_variable = 5
+
+my_boolean_variable = True
+print(my_boolean_variable)
+
+# imprimir dos variables 
+print(my_boolean_variable, my_int_variable)
+
+# transformar int a str
+my_int_2_variable =10
+print(my_int_2_variable)
+
+my_int_to_str_variable= str(my_int_2_variable)
+print(my_int_to_str_variable)
+print(type(my_int_to_str_variable))
+
+# concatenar str con valor de variable
+print("Este es el valor de:",my_int_2_variable )
+
+# Algunas funciones del sistema
+# len cuenta cuantos caracteres tiene cada variable
+print(len(my_int_to_str_variable)) # 2
+
+# variables en una sola linea
+name, sub_name, alias, age = "Pablo", "Mu", "Mumusic", 36
+print("Me llamo:",name, "mi apellido es:", sub_name, "mi alias es:", alias, "y mi edad es", age)
+
+
